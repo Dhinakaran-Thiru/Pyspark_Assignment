eb8cbcad4ff2a2d41a27a53fc50fab5a392a99a8
Dhinakaran <dhina15003@gmail.com>
2024-04-08 21:50:47 +0530

    commited the test case for assignment 5

--- test/Assignment_2/test_assignment_2.py
+++ test/Assignment_2/test_assignment_2.py
@@ -1,8 +1,85 @@
 import unittest
+from Pyspark_Assignment.src.Assignment_2.util import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestAssignment2(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.spark = SparkSession.builder.appName("test_case_2").getOrCreate()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
+
+    def test_create_df(self):
+        test_credit_card_data = [("1234567891234567",),
+                                 ("5678912345671234",),
+                                 ("9123456712345678",),
+                                 ("1234567812341122",),
+                                 ("1234567812341342",)]
+        test_credit_card_custom_schema = StructType([
+            StructField("card_number", StringType(), True)
+        ])
+        df = create_df(self.spark, test_credit_card_data, test_credit_card_custom_schema)
+        self.assertEqual(df.count(), len(test_credit_card_data))
+
+    def test_create_df_csv(self):
+        path_csv = r"C:\Users\DHINAKARAN\Documents\assignment2.csv"
+        credit_schema = StructType([
+            StructField("card_number", StringType(), True)
+        ])
+        df = create_df_csv(self.spark, path_csv, credit_schema)
+        test_credit_card_data = [("1234567891234567",),
+                                 ("5678912345671234",),
+                                 ("9123456712345678",),
+                                 ("1234567812341122",),
+                                 ("1234567812341342",)]
+        test_credit_card_custom_schema = StructType([
+            StructField("card_number", StringType(), True)
+        ])
+        expected_df = create_df(self.spark, test_credit_card_data, test_credit_card_custom_schema)
+        self.assertEqual(df.collect(), expected_df.collect())
+
+    def test_create_df_json(self):
+        # Assuming you have a JSON file named "test_data.json"
+        path_json = r"C:\Users\DHINAKARAN\Documents\assignment2.json"
+        df = create_df_json(self.spark, path_json)
+        test_credit_card_data = [("1234567891234567",),
+                                 ("5678912345671234",),
+                                 ("9123456712345678",),
+                                 ("1234567812341122",),
+                                 ("1234567812341342",)]
+        test_credit_card_custom_schema = StructType([
+            StructField("card_number", StringType(), True)
+        ])
+        expected_df = create_df(self.spark, test_credit_card_data, test_credit_card_custom_schema)
+        self.assertIsNotNone(df.collect(), expected_df.collect())
+
+    def test_get_no_of_partitions(self):
+        df = create_df(self.spark, credit_card_data, credit_card_schema)
+        partitions = get_no_of_partitions(df)
+        self.assertEqual(partitions, 4)
+
+    def test_increase_partition_by_5(self):
+        df = create_df(self.spark, credit_card_data, credit_card_schema)
+        initial_partitions = get_no_of_partitions(df)
+        new_partitions = increase_partition_by_5(df)
+        self.assertEqual(new_partitions, initial_partitions + 5)
+
+    def test_decrease_partition_by_5(self):
+        df = create_df(self.spark, credit_card_data, credit_card_schema)
+        final_partitions = decrease_partition_by_5(df)
+        self.assertEqual(final_partitions, 4)
+
+    def test_masked_card_number_udf(self):
+        df = create_df_custom_schema(self.spark, credit_card_data, credit_card_custom_schema)
+        df = df.withColumn("masked_number", masked_card_number_udf(df["card_number"]))
+        masked_numbers = df.select("masked_number").rdd.flatMap(lambda x: x).collect()
+        expected_masked_numbers = ['************4567', '************1234', '************5678',
+                                   '************1122', '************1342']
+        self.assertEqual(masked_numbers, expected_masked_numbers)
+
 
 if __name__ == '__main__':
     unittest.main()
--- test/assignment_1/assignment_test_1.py
+++ test/assignment_1/assignment_test_1.py
@@ -1,10 +1,31 @@
+from Pyspark_Assignment.src.Assignment_1.util import *  # Assuming updated functions are in this module
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestAssignment1(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.spark = SparkSession.builder.appName("PySpark Assignment").getOrCreate()
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
+
+    def test_create_dataframe(self, ):
+        test_transaction_schema = StructType([
+            StructField("buyer_id", IntegerType(), True),
+            StructField("item_model", StringType(), True)
+        ])
+        test_transaction_data = [(1, "iphone13"), (1, "dell_i5"), (2, "iphone13"), (2, "dell_i5"),
+                                  (3, "iphone13"), (3, "dell_i5"), (1, "dell_i3"), (1, "hp_i5"),
+                                  (1, "iphone14"), (3, "iphone14"), (4, "iphone13")]
+        test_product_info_schema = StructType([StructField("product_model", StringType(), True)])
+        test_product_info_data = [("iphone13",), ("dell_i5",), ("dell_i3",), ("hp_i5",), ("iphone14",)]
+        test_transaction_df = create_data_frame(self.spark,purchase_data, purchase_schema)
+        test_product_info_df = create_data_frame(self.spark, test_product_info_data, test_product_info_schema)
+
+        self.assertEqual(test_transaction_df.count(), 11)
+        self.assertEqual(test_product_info_df.count(), 5)
 
 if __name__ == '__main__':
     unittest.main()
